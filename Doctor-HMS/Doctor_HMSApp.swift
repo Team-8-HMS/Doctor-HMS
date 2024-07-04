a73e19b4255c28c70a78dd9a1f0a877e40cbef80
Krsna <krushna2483@gmail.com>
2024-07-04 10:02:03 +0530

    Initial Commit

---
+++ Doctor-HMS/Doctor_HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_HMSApp.swift
+//  Doctor-HMS
+//
+//  Created by Krsna Sharma on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
